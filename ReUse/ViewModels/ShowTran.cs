ee1f5be8dd51d58671f25e54d2923a4ec7ad1204
chenglingr <286863096@qq.com>
2016-06-11 12:44:58 +0800

    3

---
+++ ReUse/ViewModels/ShowTran.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReUse.ViewModels
+{
+    public class ShowTran
+    {
+        public string name { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string weixin{ get; set; }
+        public string tel { get; set; }
+        public string content { get; set; }
+        public DateTime createdate { get; set; }
+    }
+}
